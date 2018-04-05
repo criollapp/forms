bc40503dd2ac9960d9040f451299af2fcd8d0e67
Miguel Rivera <miguel@smsysonline.com>
2018-04-05 19:45:38 -0400

    CAFormItemAbstract on export added

--- src/library/ca-forms.ts
+++ src/library/ca-forms.ts
@@ -1,3 +1,4 @@
+export { CAFormItemAbstract } from "./abstracts/ca-form-item.abstract";
 export { CAFormComponent } from "./components/form/ca-form.component";
 export { CAFormControl } from "./class/ca-form-control.class";
 export { CAFormGroup } from "./class/ca-form-group.class";
